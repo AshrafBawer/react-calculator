2b9efc338361d9ef55b7aa58e651de5b77a8784a
Ashraf Bawer <ashraf.bawer1@gmail.com>
2020-01-21 17:48:41 +0430

    Updates

---
+++ precache-manifest.a0ed9070378fa9589f593c7943a55eb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06fe569656c251c466074be6c1d70545",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "0be4337b5f6c81d5d634",
+    "url": "/react-calculator/static/css/main.1a87c6cd.chunk.css"
+  },
+  {
+    "revision": "69266ee9fec05a39af7a",
+    "url": "/react-calculator/static/js/2.36e0c976.chunk.js"
+  },
+  {
+    "revision": "0be4337b5f6c81d5d634",
+    "url": "/react-calculator/static/js/main.bc4d8ca5.chunk.js"
+  },
+  {
+    "revision": "ea3cf0703970cad9ced4",
+    "url": "/react-calculator/static/js/runtime-main.616c395e.js"
+  }
+]);
